dba79011e5e90238ce4695610dc4ab05a363eecf
Mark <crazyeight68@gmail.com>
2018-09-19 07:34:07 -0700

    Initial commit.

---
+++ src/renderer/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'r6-stacker'
+  }
+</script>
+
+<style>
+  /* CSS */
+</style>
